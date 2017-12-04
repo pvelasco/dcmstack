1e47c420c279d936ed0053cc009b055512b3c1b5
pvelasco <pablo.velasco@nyu.edu>
2017-12-04 13:36:44 -0500

    Update info.py
    Normalized version.

--- src/dcmstack/info.py
+++ src/dcmstack/info.py
@@ -7,7 +7,7 @@ _version_major = 0
 _version_minor = 7
 _version_micro = 0
 _version_extra = 'dev'
-__version__ = "%s.%s.%s%s" % (_version_major,
+__version__ = "%s.%s.%s.%s" % (_version_major,
                               _version_minor,
                               _version_micro,
                               _version_extra)
